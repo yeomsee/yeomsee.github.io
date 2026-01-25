8bbe3891ddfcfe7e49e9b40907efcafe8fee25d1
yeomsee <114807871+yeomsee@users.noreply.github.com>
2026-01-25 15:52:30 +0000

    Deploying to gh-pages from @ yeomsee/portfolio@3724050171c9d8bee82c474b92a4a342b299d834 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "yeomsee/portfolio",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
