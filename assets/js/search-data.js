a91892091170855e4bcdc96711959e55f74182ae
yeomsee <114807871+yeomsee@users.noreply.github.com>
2026-01-28 17:39:19 +0000

    Deploying to gh-pages from @ yeomsee/yeomsee.github.io@578711b3936a43e846f86e3438c1c267359d7bbc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -249,17 +249,6 @@ Efficient LLM Sampling <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="
             window.open("https://velog.io/@yeomsee97/%EB%85%BC%EB%AC%B8%EB%A6%AC%EB%B7%B0-Retrieval-Augmented-Generation-for-Knowledge-Intensive-NLP-Tasks", "_blank");
           
         },
-      },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
       },{id: "post-a-post-with-tabs",
         
           title: "a post with tabs",
