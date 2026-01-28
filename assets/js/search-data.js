3b04a9331b05295e6263249d7dea4115a401f166
yeomsee <114807871+yeomsee@users.noreply.github.com>
2026-01-28 18:18:14 +0000

    Deploying to gh-pages from @ yeomsee/yeomsee.github.io@f77ca1886e2977ad9c2f3a29159c0f7c48718858 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "post-efficient-tts-서베이",
         
           title: 'Efficient TTS 서베이 <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
