a95bc396e627a35c9cee0612feaac9bdfdc14118
yeomsee <114807871+yeomsee@users.noreply.github.com>
2026-02-21 08:00:25 +0000

    Deploying to gh-pages from @ yeomsee/yeomsee.github.io@b0229167887bb0da5a2ea9f023209dd21e9cb589 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -376,16 +376,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
